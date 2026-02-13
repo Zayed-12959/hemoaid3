eb147b2efcf9a85502f79e83818b729ecd01cf65
Zayed-12959 <zayed12959@gmail.com>
2026-02-13 15:04:39 +0600

    First Push

---
+++ android/app/src/main/kotlin/com/example/hemoaid3/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hemoaid3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/welcome.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+class Welcome extends StatelessWidget {
+  const Welcome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text("Welcome", style: TextStyle(color: Colors.white),),
+        centerTitle: true,
+        //leading: IconButton(onPressed: () {}, icon: Icon(Icons.person_2_outlined, color: Colors.white,)),
+        // actions: [
+        //   IconButton(
+        //       onPressed: () {},
+        //       icon: Icon(Icons.menu_outlined, color: Colors.white,))
+        // ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Hemo Aid",
+              style: TextStyle(
+                fontSize: 45,
+                fontWeight: FontWeight.bold,
+                color: Colors.red,
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 50, right: 50,bottom: 10,top: 30),
+              child: TextField(
+                style: TextStyle(
+                  color: Colors.blue,
+                ),
+                decoration: InputDecoration(
+                    label: Text(
+                      "Username",
+                      style: TextStyle(
+                          color: Colors.blue
+                      ),
+                    ),
+                    suffixIcon: Icon(
+                        Icons.person_2
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Colors.red
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(40))
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Colors.red
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(40))
+                    )
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 50, right: 50),
+              child: TextField(
+                style: TextStyle(
+                  color: Colors.blue,
+                ),
+                decoration: InputDecoration(
+                    label: Text(
+                      "Password",
+                      style: TextStyle(
+                          color: Colors.blue
+                      ),
+                    ),
+                    suffixIcon: Icon(
+                        Icons.key_outlined
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Colors.red
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(40))
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Colors.red
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(40))
+                    )
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 30),
+              child: ElevatedButton(onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      elevation: 10,
+                      backgroundColor: Colors.red,
+                      fixedSize: Size(100,20)
+                  ),
+
+                  child: Text("Log in", style: TextStyle(color: Colors.white),)),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
